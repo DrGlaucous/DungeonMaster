6f1adb1739926ca583be7b4037b46d533b39474f
Dr. Glaucous <49349519+DrGlaucous@users.noreply.github.com>
2024-09-20 17:37:43 -0400

    begin migration to wpf

--- Software/Tests/WpfApp1/MainWindow.xaml.cs
+++ Software/Tests/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +21,71 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private System.Timers.Timer timer_minor = new(10);
+        private int value = 0;
+
+        static bool aa = false;
+
+        private void MyMethod(object sender, RoutedEventArgs e)
+        {
+            if ((string)buttonn.Content == "APPLE")
+            {
+                buttonn.Content = "PEAR";
+
+                if (!aa)
+                {
+                    timer_minor.Elapsed += IncrementStopwatch;
+                    timer_minor.AutoReset = true;
+                    timer_minor.Enabled = true;
+                    timer_minor.Stop();
+                    aa = true;
+                }
+
+                StartStopwatch();
+            }
+            else
+            {
+                buttonn.Content = "APPLE";
+                StopStopwatch();
+            }
+
+        }
+
+        //starts counting up the stopwatch
+        public void StartStopwatch()
+        {
+            timer_minor.Start();
+        }
+
+        //stops counting up the stopwatch
+        public void StopStopwatch()
+        {
+            timer_minor.Stop();
+        }
+
+        //ticks the stopwatch up
+        private void IncrementStopwatch(Object? source, ElapsedEventArgs e)
+        {
+            value += 1;
+            log_left_accs(value);
+        }
+
+        //update stopwatch to display timespan from outside thread
+        delegate void ParametrizedMethodInvoker5(int arg);
+        void log_left_accs(int arg)
+        {
+            if (!Dispatcher.CheckAccess()) // CheckAccess returns true if you're on the dispatcher thread
+            {
+                Dispatcher.Invoke(new ParametrizedMethodInvoker5(log_left_accs), arg);
+                return;
+            }
+            StopwatchLabel.Content = arg.ToString();
+        }
+
+
+
     }
+
+
 }
