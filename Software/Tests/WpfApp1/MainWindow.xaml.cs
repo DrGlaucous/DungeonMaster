d30198f8b75321f3b36163344822366c9dcf9805
Edward Stuckey <observer21.public@gmail.com>
2024-10-27 01:14:14 -0400

    Work on CAD parts and testing new serial backend

--- Software/Tests/WpfApp1/MainWindow.xaml.cs
+++ Software/Tests/WpfApp1/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RJCP.IO.Ports;
 
 namespace WpfApp1
 {
@@ -17,73 +18,63 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SerialPortStream sps = new SerialPortStream();
+       // private SerialPort sp = new SerialPort();
+
+        //when we get new data
+        public delegate void StringDelegate(string arg);
+        //public event StringDelegate? DataGetEventHandler;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            sps.DataReceived += new EventHandler<SerialDataReceivedEventArgs>(SerialGetCb);
+
+
+            int baud = 115200;
+            sps.BaudRate = baud;
+            sps.PortName = "COM3";
+            sps.Open();
         }
 
-        private System.Timers.Timer timer_minor = new(10);
-        private int value = 0;
-
-        static bool aa = false;
-
-        private void MyMethod(object sender, RoutedEventArgs e)
+        private void SendButton_Click(object sender, RoutedEventArgs e)
         {
-            if ((string)buttonn.Content == "APPLE")
-            {
-                buttonn.Content = "PEAR";
-
-                if (!aa)
-                {
-                    timer_minor.Elapsed += IncrementStopwatch;
-                    timer_minor.AutoReset = true;
-                    timer_minor.Enabled = true;
-                    timer_minor.Stop();
-                    aa = true;
-                }
-
-                StartStopwatch();
-            }
-            else
-            {
-                buttonn.Content = "APPLE";
-                StopStopwatch();
-            }
-
+            sps.Write("<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA<AAA");
         }
 
-        //starts counting up the stopwatch
-        public void StartStopwatch()
-        {
-            timer_minor.Start();
+
+        private void SerialGetCb(object? sender, SerialDataReceivedEventArgs e) {
+            string data = sps.ReadExisting();
+            WriteToWindow(data);
         }
 
-        //stops counting up the stopwatch
-        public void StopStopwatch()
-        {
-            timer_minor.Stop();
-        }
+        private void WriteToWindow(string new_text) {
 
-        //ticks the stopwatch up
-        private void IncrementStopwatch(Object? source, ElapsedEventArgs e)
-        {
-            value += 1;
-            log_left_accs(value);
-        }
-
-        //update stopwatch to display timespan from outside thread
-        delegate void ParametrizedMethodInvoker5(int arg);
-        void log_left_accs(int arg)
-        {
-            if (!Dispatcher.CheckAccess()) // CheckAccess returns true if you're on the dispatcher thread
-            {
-                Dispatcher.Invoke(new ParametrizedMethodInvoker5(log_left_accs), arg);
+            if (!Dispatcher.CheckAccess()) {
+                Dispatcher.Invoke(new StringDelegate(WriteToWindow), new_text);
                 return;
             }
-            StopwatchLabel.Content = arg.ToString();
+
+            //normalize carriage returns
+            new_text = new_text.Replace("\r\n", "\n").Replace("\n", Environment.NewLine);
+
+            //get length for size buffering
+            var more_len = new_text.Length;
+            var curr_len = SerialInbox.Text.Length;
+
+            SerialInbox.Text += new_text; //add text (edge case: could potentially run out of memory here!)
+
+            //if > 0, we've added too much, crop to length
+            var len_diff = curr_len + more_len - 2000;
+            if (len_diff > 0)
+            {
+                SerialInbox.Text = SerialInbox.Text[len_diff..];
+            }
+
+            ScrollContainer.ScrollToBottom();
+
         }
-
-
 
     }
 
